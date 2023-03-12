8281a9b3c2f686dedaa9a4dae6d8b2a8f75fcc5b
olegrix2222 <125788873+olegrix2222@users.noreply.github.com>
2023-03-12 19:58:52 +0000

    ErRoR cOdE+

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import requests
 from colorama import Fore
  
-  def print("You"):
-    print(Fore.RED)
-    print("You")
+def print(You):
+  print(Fore.RED)
+  print("You")
 
-  def print("Tube"):
-    print(Fore.WHITE)
-    print("Tube")
+def print(Tube):
+  print(Fore.WHITE)
+  print("Tube")
     
 
                                                                
