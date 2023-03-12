381499cff9c64ccebe58e6ee460b2739b5932a23
olegrix2222 <125788873+olegrix2222@users.noreply.github.com>
2023-03-12 19:53:53 +0000

    ErRoR cOdE

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+from colorama import Fore
+ 
+  def print("You"):
+    print(Fore.RED)
+    print("You")
+
+  def print("Tube"):
+    print(Fore.WHITE)
+    print("Tube")
+    
+
+                                                               
